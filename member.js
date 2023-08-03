00b881a354d5bb3910cb76219fede4b7e99ac5eb
hanpera <hanpera@gmail.com>
2023-08-03 13:54:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillMembers() {
+  const members = [
+    {
+      name: 'John',
+      skill: 'JavaScript',
+    },
+    {
+      name: 'Jane',
+      skill: 'TypeScript',
+    },
+    {
+      name: 'Joe',
+      skill: 'Java',
+    },
+  ]
+
+  return members
+}
